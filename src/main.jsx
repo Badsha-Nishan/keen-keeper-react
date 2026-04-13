1e580e31b0dcad0fba5a10891d94c515beac8231
Badsha-Nishan <badshanisan14@gmail.com>
2026-04-13 23:14:23 +0600

    added the layout structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "./layout/RootLayout";
+import Homepage from "./pages/homepage/Homepage";
+import TimeLinePage from "./pages/timelinepage/TimeLinePage";
+import StatsPage from "./pages/statspage/StatsPage";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Homepage,
+      },
+      {
+        path: "/timeline",
+        Component: TimeLinePage,
+      },
+      {
+        path: "/stats",
+        Component: StatsPage,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
