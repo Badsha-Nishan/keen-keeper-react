7c10e84e78cb732d8dc8084a5016e9f9dea8b030
Badsha-Nishan <badshanisan14@gmail.com>
2026-04-13 23:35:31 +0600

    implement Navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,11 +1,71 @@
-import React from 'react';
+import React from "react";
+import Logo from "../../assets/logo.png";
+import { GoHome } from "react-icons/go";
+import { RiTimeLine } from "react-icons/ri";
+import { ImStatsDots } from "react-icons/im";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h2>This is navbar</h2>
+  return (
+    <div className="bg-base-100 shadow-sm">
+      <div className="navbar w-11/12 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost sm:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <a className="btn">
+                {" "}
+                <GoHome /> Home
+              </a>
+              <a className="btn">
+                {" "}
+                <RiTimeLine /> Timeline
+              </a>
+              <a className="btn">
+                {" "}
+                <ImStatsDots /> Stats
+              </a>
+            </ul>
+          </div>
+          <a className="text-xl">
+            <img src={Logo} alt="keen keeper logo" />
+          </a>
         </div>
-    );
+        <div className="hidden sm:flex navbar-end gap-3">
+          <a className="btn">
+            {" "}
+            <GoHome /> Home
+          </a>
+          <a className="btn">
+            {" "}
+            <RiTimeLine /> Timeline
+          </a>
+          <a className="btn">
+            {" "}
+            <ImStatsDots /> Stats
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
