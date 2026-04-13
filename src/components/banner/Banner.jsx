bfcd69599db6aebf7e623e35795fff691fb9f166
Badsha-Nishan <badshanisan14@gmail.com>
2026-04-13 23:42:49 +0600

    implement Banner Section

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { IoMdAdd } from "react-icons/io";
+
+const Banner = () => {
+  return (
+    <div className="w-11/12 mx-auto text-center space-y-5 mt-14">
+      <h2 className="text-4xl font-bold">Friends to keep close in your life</h2>
+      <p>
+        Your personal shelf of meaningful connections. Browse, tend, and nurture
+        the <br /> relationships that matter most.
+      </p>
+      <button className="btn bg-[#244D3F] text-white">
+        {" "}
+        <IoMdAdd />
+        Add a Friend
+      </button>
+    </div>
+  );
+};
+
+export default Banner;
